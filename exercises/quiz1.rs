6eb4271b22f681de3b028c876d7684fe9a1698fb
Jiang Sheng <jiangshengdev@outlook.com>
2025-02-10 18:03:06 +0800

    16

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn calculate_price_of_apples {
+
+fn calculate_price_of_apples(nums: i32) -> i32 {
+    if nums > 40 {
+        return nums
+    } else {
+        return nums * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
